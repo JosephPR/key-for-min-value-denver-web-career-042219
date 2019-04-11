e4eea2ffeb5ec530662da3d6b20b5a6c90170c13
Joseph Reilly <reilly4181@gmail.com>
2019-04-11 16:35:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  number = 1
+  number = 1000000000000000
   name_hash.collect do |name, value|
     if value < number
       number = value
